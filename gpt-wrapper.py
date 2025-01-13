f6fb67017062498caac14f1b96c0c7bc5b429763
Priscilla Ye <34380354+ScriptKitKat@users.noreply.github.com>
2025-01-12 18:04:08 -0600

    Update gpt-wrapper.py

--- gpt-wrapper.py
+++ gpt-wrapper.py
@@ -1,13 +1,31 @@
+# This file will serve as our GPT connection
 from openai import OpenAI
 import streamlit as st
+import requests
 
 api_key = st.secrets["api_key"]
-
 client = OpenAI(api_key=api_key)
-completion = client.Completion.create(
-    model="gpt-3.5-turbo",
-    store=True,
-    messages=[
-        {"role": "system", "content": "You are a helpful assistant."},
-    ]
-)
+
+def generate_text(prompt):
+    completion = client.chat.completions.create(
+        model="gpt-4o",
+        messages=[
+            {"role": "user", "content": prompt}
+        ]
+    )
+    return completion.choices[0].message.content
+
+def generate_image(prompt):
+    response = client.images.generate(
+        model="dall-e-3",
+        prompt=prompt,
+        size="1024x1024",
+        quality="standard",
+        n=1,
+    )
+    image_url = response.data[0].url
+
+    image_response = requests.get(image_url)
+
+    with open("otter.png", 'wb') as file:
+        file.write(image_response.content)
